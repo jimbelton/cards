505ef97b08d84fb89425ad0f448a717543865c5e
jimbelton <jim.belton@gmail.com>
2018-09-01 12:52:21 -0700

    Stub Score and ToStr

--- blackjack/hand.go
+++ blackjack/hand.go
@@ -14,6 +14,20 @@ func NewHand(deck *cards.Deck) *Hand {
     return hand
 }
 
-func (hand *Hand) NumCards() int {
-    return len(hand.cards)
+func (hand *Hand) Score() int {
+    return 4
+}
+
+func (hand *Hand) ToStr() string {
+    str := ""
+
+    for i := 0; i < len(hand.cards); i++ {
+        str += hand.cards[i].Rank + "/" + hand.cards[i].Suit
+
+        if i < len(hand.cards) - 1 {
+            str += ", "
+        }
+    }
+
+    return str
 }
--- blackjack_test.go
+++ blackjack_test.go
@@ -8,7 +8,7 @@ func TestHand(t *testing.T) {
     deck := cards.NewDeck()
     hand := blackjack.NewHand(deck)
 
-    if hand.NumCards() != 2 {
-        t.Errorf("A blackjack hand should have 2 cards, not %d", hand.NumCards())
+    if hand.Score() != 4 {
+        t.Errorf("Blackjack hand %s should have a score of 4, not %d", hand.ToStr(), hand.Score())
     }
 }
